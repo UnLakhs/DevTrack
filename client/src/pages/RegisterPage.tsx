cc1a73824e7df26d86ecad8cd498be19473957ea
UnLakhs <apostolos052@gmail.com>
2026-04-28 00:19:05 +0300

    added logo

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import Logo from "../components/Logo";
 
 export default function RegisterPage() {
   const navigate = useNavigate();
@@ -51,9 +52,17 @@ export default function RegisterPage() {
 
   return (
     <main className="flex min-h-screen items-center justify-center bg-zinc-950 px-4 text-white">
-      <div className="w-full max-w-md rounded-2xl border border-zinc-800 bg-zinc-900 p-8 shadow-lg">
+      {/* BackGround Styles */}
+      <div className="pointer-events-none absolute inset-0">
+        <div className="absolute -top-50 -left-50 h-125 w-125 rounded-full bg-emerald-500/10 blur-3xl" />
+        <div className="absolute -bottom-50 -right-50 h-125 w-125 rounded-full bg-cyan-500/10 blur-3xl" />
+      </div>
+      <div className="pointer-events-none absolute inset-0 bg-[linear-gradient(to_right,rgba(255,255,255,0.03)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.03)_1px,transparent_1px)] bg-size-[40px_40px]" />
+
+      {/* Register Box */}
+      <div className="w-full z-10 max-w-md rounded-2xl border border-zinc-800 bg-zinc-900 p-8 shadow-lg">
         <div className="mb-8">
-          <h1 className="text-3xl font-bold tracking-tight">DevTrack</h1>
+          <Logo />
           <p className="mt-2 text-sm text-zinc-400">
             Create an account to manage your projects and tasks.
           </p>
